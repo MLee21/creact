1d0e9f49acead265b2114a8d0a1a4606f6c51c92
MLee21 <minnie.lee89@gmail.com>
2016-01-12 19:37:48 -0700

    can level up or level down.

--- app/assets/javascripts/components/_skill.js.jsx
+++ app/assets/javascripts/components/_skill.js.jsx
@@ -15,6 +15,21 @@ var Skill = React.createClass({
     }
     this.setState({ editable: !this.state.editable })
   },
+  handleLevelChange(action) {
+    let levels = ['bad', 'halfbad', 'fantastic'];
+    let name = this.props.skill.name;
+    let details = this.props.skill.details;
+    let level = this.props.skill.level;
+    let index = levels.indexOf(level);
+
+    if (action === 'up' && index < 2) {
+      let newLevel = levels[index + 1];
+      this.props.handleUpdate({id: this.props.skill.id, name: name, details: details, level: newLevel })
+    } else if (action === 'down' && index > 0) {
+      let newLevel = levels[index - 1];
+      this.props.handleUpdate({id: this.props.skill.id, name: name, details: details, level: newLevel })
+    }
+  },
 render() {
   var name = this.state.editable ? <input type='text' ref='name' defaultValue={this.props.skill.name} />
                                  : <h3>{this.props.skill.name}</h3>
@@ -22,11 +37,20 @@ render() {
   let details = this.state.editable ? <textarea type='text' ref='details' defaultValue={this.props.skill.details}></textarea>
                                     : <p>{this.props.skill.details}</p>
     return (
-      <div>
+      <div className='skill-level'>
         {name}
 
         <p><strong>Level:</strong> {this.props.skill.level}</p>
+        
         {details}
+
+        <button type="button" className="btn btn-default btn-sm" onClick={ this.handleLevelChange.bind(this, 'down')}>
+          <span className="glyphicon glyphicon-triangle-bottom"></span>
+        </button>
+
+        <button type="button" className="btn btn-default btn-sm" onClick={ this.handleLevelChange.bind(this, 'up')}>
+          <span className="glyphicon glyphicon-triangle-top"></span>
+        </button>
 
         <button onClick={this.props.handleDelete}>
           Delete
